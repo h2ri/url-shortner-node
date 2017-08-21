e64bedc982c0dc1ed1de1672e61b64c63c8ba7f0
Hari <hariprasad@pharmeasy.in>
2017-08-21 16:43:03 +0530

    removed comment

--- App.js
+++ App.js
@@ -1,25 +1,16 @@
 var React = require('react'),
     DOM = React.DOM, div = DOM.div, button = DOM.button, ul = DOM.ul, li = DOM.li, input = DOM.input
 var $ = require('jquery')
-// This is just a simple example of a component that can be rendered on both
-// the server and browser
 
 module.exports = React.createClass({
 
-  // We initialise its state by using the `props` that were passed in when it
-  // was first rendered. We also want the button to be disabled until the
-  // component has fully mounted on the DOM
   getInitialState: function() {
-    return {items: this.props.items, disabled: true, shortUrls: [], urlInput: 'www.youtube.com'}
+    return {items: this.props.items, disabled: true, shortUrls: [], urlInput: 'https://www.youtube.com/'}
   },
 
-  // Once the component has been mounted, we can enable the button
   componentDidMount: function() {
     this.setState({disabled: false,shortUrls:[]})
   },
-  // Then we just update the state whenever its clicked by adding a new item to
-  // the list - but you could imagine this being updated with the results of
-  // AJAX calls, etc
   handleClick: function() {
     this.setState({
       items: this.state.items.concat('Item ' + this.state.items.length)
@@ -48,10 +39,6 @@ module.exports = React.createClass({
      })
    },
 
-  // For ease of illustration, we just use the React JS methods directly
-  // (no JSX compilation needed)
-  // Note that we allow the button to be disabled initially, and then enable it
-  // when everything has loaded
   render: function() {
     return div({style: {padding:'50px'}},
       div({style:{
@@ -60,7 +47,7 @@ module.exports = React.createClass({
           fontFamily: 'sans-serif',
           padding: '15px'
         }
-      },'Header' ),
+      },'Url Shotner' ),
       input({
         style: {
           width: '100%',
